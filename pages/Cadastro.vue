b08d91c6593d16cade245e993a1f863c40e2f6ad
lipeqalves <lipeqa@gmail.com>
2022-10-20 21:39:22 -0300

    first commit

---
+++ pages/Cadastro.vue
@@ -0,0 +1,109 @@
+
+<template>
+    <v-card class="pa-6">
+        <v-toolbar color="white" flat>
+            <v-btn icon light>
+                <v-icon color="grey darken-2">
+                    mdi-arrow-left
+                </v-icon>
+            </v-btn>
+
+            <v-toolbar-title class="grey--text text--darken-4">
+                Cadastro
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+        </v-toolbar>
+        <v-form ref="form" v-model="valid" lazy-validation class="ml-4">
+            <v-card-title class="headline">
+                Cadastro Cliente
+            </v-card-title>
+
+
+            <v-text-field v-model="clientes.name" :rules="nameRules" label="Name" required></v-text-field>
+
+            <v-text-field v-model="clientes.email" :rules="emailRules" label="E-mail" required></v-text-field>
+
+            <v-text-field v-model="clientes.telefone" label="Telefone" required></v-text-field>
+
+            <v-text-field v-model="clientes.cnh" label="CNH" required></v-text-field>
+
+            <v-card-title class="headline">
+                Cadastro Veiculo
+            </v-card-title>
+
+
+            <v-text-field v-model="veiculos.veiculo" label="Veiculo" required></v-text-field>
+
+            <v-text-field v-model="veiculos.placa" label="Placa" required></v-text-field>
+
+            <v-text-field v-model="veiculos.modelo" label="Modelo" required></v-text-field>
+
+            <v-text-field v-model="veiculos.ano" label="Ano" required></v-text-field>
+
+            <v-text-field v-model="veiculos.cor" label="cor" required></v-text-field>
+
+            <v-btn color="success" class="mr-4" @click="enviar">
+                Enviar
+            </v-btn>
+
+            <pre>
+            {{clientes}}
+        </pre>
+            <pre>
+            {{veiculos}}
+        </pre>
+        </v-form>
+        <v-footer class="mt-12"></v-footer>
+    </v-card>
+
+</template>
+<script>
+export default {
+    data: () => ({
+        valid: true,
+        clientes: {
+            name: '',
+            email: '',
+            telefone: '',
+            cnh: '',
+        },
+        veiculos: {
+            veiculo: '',
+            placa: '',
+            modelo: '',
+            ano: '',
+            cor: "",
+        },
+        nameRules: [
+            v => !!v || 'Name is required',
+            v => (v && v.length > 3) || 'nome tem que ter mais de 3 caracteres',
+        ],
+        emailRules: [
+            v => !!v || 'E-mail is required',
+            v => /.+@.+\..+/.test(v) || 'formato do email teste@test.com.br',
+        ],
+    }),
+
+    methods: {
+        
+        async enviar() {
+
+            debugger
+
+            try {
+                var response = await this.$axios({
+                    method: 'POST',
+                    url: 'https://localhost:7280/Clientes',
+                    data: this.clientes,
+                });
+
+                debugger
+            } catch { }
+
+        },
+
+    },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="10" sm="8" md="12">
+      <v-card class="logo py-0 d-flex justify-center">
+        <v-img height="600"
+          src="https://ocinemadavilla.pt/wp-content/uploads/2017/07/Estacionamento-1000x525.png">
+        </v-img>
+      </v-card>
+      <v-card>
+        <v-card-actions >
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/cadastro">
+            Cadastre-se
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
